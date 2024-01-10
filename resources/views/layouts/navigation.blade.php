1f911d011cee49cb5d28bf56a46eae41c2ca96dd
cuantocarlos <fco.carlos@protonmail.com>
2024-01-10 06:00:13 +0100

    Middleware Auth

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,9 +15,7 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                        {{ __('Posts') }}
-                    </x-nav-link>
+                    
                 </div>
             </div>
 
@@ -87,9 +85,7 @@
                     {{ __('Profile') }}
                 </x-responsive-nav-link>
 
-                <x-responsive-nav-link :href="route('posts.index')"><!--mio-->
-                        {{ __('Posts') }}
-                    </x-responsive-nav-link>
+              
 
                 <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,10 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('posts', [PostController::class, 'index'])->name('posts.index');
-Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('posts', [PostController::class, 'store'])->middleware(['auth', 'verified'])->name('posts.store');
+Route::delete('posts/{post}', [PostController::class, 'destroy'])->middleware(['auth', 'verified'])->name('posts.destroy');
 
 require __DIR__.'/auth.php';
