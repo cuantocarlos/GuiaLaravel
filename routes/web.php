dab9d549bc8897434f3646991b2938d480ab700d
cuantocarlos <fco.carlos@protonmail.com>
2024-01-12 14:11:30 +0100

    Filtrado de registros

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,7 @@ Route::redirect('/', 'dashboard');
 Route::get('dashboard', [PostController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::post('posts', [PostController::class, 'store'])->middleware(['auth', 'verified'])->name('posts.store');
 Route::delete('posts/{post}', [PostController::class, 'destroy'])->middleware(['auth', 'verified'])->name('posts.destroy');
+
+Route::get('user/{user}', [UserController::class, 'show'])->middleware(['auth', 'verified'])->name('users.show');
 
 require __DIR__.'/auth.php';
